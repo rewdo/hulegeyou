dafad05b9c6ff59dcce8bd900ec8533d2b541e9e
rewdo <rewdo33@163.com>
2026-05-15 16:58:13 +0800

    fix: roundRect polyfill - 微信canvas不支持原生roundRect，改用quadraticCurveTo实现

--- js/render.js
+++ js/render.js
@@ -14,3 +14,21 @@ canvas.height = screenHeight;
 export const SCREEN_WIDTH = screenWidth;
 export const SCREEN_HEIGHT = screenHeight;
 export default canvas;
+
+// roundRect polyfill（微信 canvas 不支持原生 roundRect）
+if (!CanvasRenderingContext2D.prototype.roundRect) {
+  CanvasRenderingContext2D.prototype.roundRect = function(x, y, w, h, r) {
+    if (r > w / 2) r = w / 2;
+    if (r > h / 2) r = h / 2;
+    this.moveTo(x + r, y);
+    this.lineTo(x + w - r, y);
+    this.quadraticCurveTo(x + w, y, x + w, y + r);
+    this.lineTo(x + w, y + h - r);
+    this.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+    this.lineTo(x + r, y + h);
+    this.quadraticCurveTo(x, y + h, x, y + h - r);
+    this.lineTo(x, y + r);
+    this.quadraticCurveTo(x, y, x + r, y);
+    this.closePath();
+  };
+}
